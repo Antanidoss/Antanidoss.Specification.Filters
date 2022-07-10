cc4175a8ca7ab937d9a4535b3e17d13a2cd91044
Antanidoss <antosci2002@gmail.com>
2022-07-10 09:17:39 +0300

    added FirstOrDefaultAsync filter

---
+++ src/Antanidoss.Specification.Filters/Interfaces/IQueryableSingleResultFilterAsync.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Antanidoss.Specification.Filters.Interfaces
+{
+    public interface IQueryableSingleResultFilterAsync<TEntity> : IBaseFilter<Task<TEntity>, IQueryable<TEntity>, TEntity>
+        where TEntity : class
+    {
+    }
+}
